ab273fbb127a7e94d61f0a82ae00b402017d35c5
beliyandr <beliyandr@gmail.com>
2025-08-12 16:08:52 +0300

    video

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Header from './Header.js';
 import TabsCollections from './Tabs.js';
+import VideoPlayerCollection from './VideoPlayer.js';
 
 new Header();
 new TabsCollections();
+new VideoPlayerCollection();
